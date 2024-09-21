b0ce1b3d2388f7cf094dabde165d6f947591aba4
r0ry64 <rorybeatz@gmail.com>
2024-09-20 23:38:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var studentName = "Rory"
+var studentAge = 24
+var isEnrolled = true
+console.log("My name is",studentName, "and I am",studentAge,"years old.")
+console.log("It is",isEnrolled,"that I am enrolled in the CPAN course")
+
+let string1 = "This is a string"
+let number1 = 420
+let bool1 = true
+let fruitArray = ["mango","papaya","pineapple","watermelon"]
+let person = {
+    firstName: "Edgar",
+    middleName: "Rory",
+    lastName: "Cobb",
+    age: 24
+  };
+
+console.log(string1)
+console.log(number1,"is a number")
+console.log("The value of this boolean is",bool1)
+console.log("Here are some of my favourite fruits:",fruitArray)
+console.log("My info:",person)
+
+let x = 100
+let y = 2
+console.log("100 + 2 =",x+y)
+console.log("100 - 2 =",x-y)
+console.log("100 * 2 =",x*y)
+console.log("100 / 2 =",x/y)
+console.log("100 > 2 is",x>y)
+console.log("100 < 2 is",x<y)
+console.log("100 == 2 is",x==y)
+console.log("100 != 2 is",x!=y)
+
+let boolX = true
+let boolY = false
+console.log("Can I compare true AND false?",boolX&&boolY)
+console.log("Can I compare true OR false?",boolX||boolY)
+console.log("NOT true is equal to",!boolX)
